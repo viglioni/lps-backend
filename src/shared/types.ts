04f8bd5be92e8c593673f942321dcc4dc62609f7
Laura <laura.viglioni@loft.com.br>
2021-08-18 23:31:51 -0300

    feat: added more types

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,1 +1,12 @@
+import { IncomingHttpHeaders } from 'http'
+
 export type ErrorMsg = string
+export type ApiKey = string
+export type LpYear = number
+export type Integer = number
+export type Url = string
+export type Brl = string
+
+export type Headers = IncomingHttpHeaders & {
+  api_key?: string
+}
