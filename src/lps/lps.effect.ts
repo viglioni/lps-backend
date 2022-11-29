c262aa9bac5588076f5b8f6b1fab98b7f3c2109c
Laura <viglionigr@gmail.com>
2022-11-29 17:17:14 -0300

    feat: check if lp exists before adding it

--- src/lps/lps.effect.ts
+++ src/lps/lps.effect.ts
@@ -1,3 +1,4 @@
+import * as O from 'fp-ts/Option'
 import {
   HttpEffect,
   HttpRequest,
@@ -10,9 +11,13 @@ import * as rx from 'rxjs/operators'
 import { throwBadRequest } from '../exceptions/bad-request'
 import { ioDecoder } from '../shared/decoders'
 import adapter from './lps.adapter'
-import { addLpDecoder } from './lps.decoders'
+import {
+  addLpDecoder,
+  PreSaveLpsEntity,
+} from './lps.decoders'
 import repo from './lps.repo'
 import { writeAuthMiddleware } from './middlewares/lps.auth.middleware'
+import { LPsEntity } from './entities/lps.entity'
 
 /**
  * Get all LPs from database
@@ -31,6 +36,16 @@ const decodeReq = rx.mergeMap((req: HttpRequest) =>
   ),
 )
 
+const addOrReturnLP = async (
+  lpInfo: PreSaveLpsEntity,
+): Promise<LPsEntity> => {
+  const lp = await repo.getLP(lpInfo)
+  if (O.isSome(lp)) {
+    return lp.value
+  }
+  return repo.saveLP(lpInfo)
+}
+
 /**
  * Creates new LP
  */
@@ -39,7 +54,7 @@ export const addNewLPEffect: HttpEffect = req =>
     use(writeAuthMiddleware),
     decodeReq,
     rx.map(adapter.preCreateLp),
-    rx.mergeMap(repo.saveLP),
+    rx.mergeMap(addOrReturnLP),
     rx.map(body => ({ body })),
   )
 
